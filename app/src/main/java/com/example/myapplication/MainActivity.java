cf841d1774273755befbd19d692c7e225a95bfed
Rachmat <rachmat.nur@gmail.com>
2022-09-17 11:40:40 +0700

    My Application

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnDaftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "Anda sudah terdaftar!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
